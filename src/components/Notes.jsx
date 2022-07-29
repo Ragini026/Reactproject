7462e405afbc481918aa89f40f5f62f13ef44813
Ragini Sinha <96348249+Ragini026@users.noreply.github.com>
2022-07-29 19:35:35 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is the react bootcamp</h1>
+      <p>
+        An internship is a period of work experience offered by an organization
+        for a limited period of time.{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
